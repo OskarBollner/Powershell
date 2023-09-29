3b7f57921eff30a7774eb9728fbd5d6e211274ec
Oskar Bollner <oskbo133@student.liu.se>
2023-09-29 12:34:33 +0200

    Added file for PS profile.

---
+++ profile.ps1
@@ -0,0 +1,48 @@
+﻿
+
+## Runs g++ with flags ##
+
+function g++_flags {g++ -std=c++17 -Wall -Wextra -pedantic -Weffc++ -Wold-style-cast -Woverloaded-virtual -fmax-errors=3 -g $args[0]}
+
+Set-Alias -Name gpp -Value g++_flags
+
+
+## Reloads Powershell ##
+function Invoke-PowerShell {
+    powershell –nologo
+    Invoke-PowerShell
+}
+
+function Restart-PowerShell {
+    if ($host.Name -eq 'ConsoleHost') {
+        exit
+    }
+    Write-Warning 'Only usable while in the PowerShell console host'
+}
+
+Set-Alias –Name 'reload' –Value 'Restart-PowerShell'
+
+$parentProcessId = (Get-WmiObject Win32_Process –Filter "ProcessId=$PID").ParentProcessId
+$parentProcessName = (Get-WmiObject Win32_Process –Filter "ProcessId=$parentProcessId").ProcessName
+
+if ($host.Name -eq 'ConsoleHost') {
+    if (-not($parentProcessName -eq 'powershell.exe')) {
+        Invoke-PowerShell
+    }
+}
+
+
+## Displays current git username or change if argument != null ##
+function gcun {git config --global user.name $args[0]}
+
+Set-Alias -Name acc_name -Value gcun
+
+
+## Displays current git email or change if argument != null ##
+function gcue {git config --global user.email $args[0]}
+
+Set-Alias -Name acc_email -Value gcue
+
+
+## Imports posh-git ##
+Import-Module posh-git
